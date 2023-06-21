3aa140d9f2b540732d23e653f2f86d19390e84ed
Panzer0325 <131454988+Panzer0325@users.noreply.github.com>
2023-06-21 12:53:55 +0800

    Delete 2023OopFinalProject-master directory

--- 2023OopFinalProject-master/src/evRentingPlatformGUI/Image.java
+++
@@ -1,9 +0,0 @@
-package evRentingPlatformGUI;
-
-import javax.swing.ImageIcon;
-
-
-public class Image {
-	ImageIcon ridingicon =new ImageIcon("ride.gif");
-	ImageIcon pauseicon =new ImageIcon("pause.jpg");
-}
