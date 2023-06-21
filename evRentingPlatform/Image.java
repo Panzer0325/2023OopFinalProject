b95acd312c183f01690a97147e7fa26235534472
Panzer0325 <131454988+Panzer0325@users.noreply.github.com>
2023-06-21 10:47:03 +0800

    Delete evRentingPlatform directory

--- evRentingPlatform/Image.java
+++
@@ -1,9 +0,0 @@
-package evRentingPlatform;
-
-import javax.swing.ImageIcon;
-
-public class Image {
-	public ImageIcon scooter=new ImageIcon("scooter.gif");
-	public ImageIcon menu=new ImageIcon("menu.gif");
-	;
-}
